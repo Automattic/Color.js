b3826aa13b7903f180825bed6795bfd1347e2f40
Matt Wiebe <matt@somadesign.ca>
2012-08-26 12:39:57 -0500

    Some light error handling
    I don't like try/catch in JS

--- src/color.js
+++ src/color.js
@@ -10,6 +10,7 @@
 	Color.prototype = {
 		_color: 0,
 		_alpha: 1,
+		error: false,
 		// for preserving hue/sat in fromHsl().toHsl() flows
 		__hsl: { h: 0, s: 0, l: 0 },
 
@@ -36,6 +37,7 @@
 
 		fromCSS: function( color ) {
 			var nums, list;
+			this.error = false;
 			if ( color.match(/^(rgb|hsl)a?/) ) {
 				list = color.replace(/(\s|%)/g, '').replace(/^(rgb|hsl)a?\(/, '').replace(/\);?$/, '').split(',');
 				if ( list.length === 4 ) {
@@ -63,18 +65,31 @@
 		},
 
 		fromRgb: function( rgb, preserve ) {
+			if ( typeof rgb !== 'object' && ( ! rgb.r || ! rgb.g || ! rgb.b ) ) {
+				this.error = true;
+				return this;
+			}
+			this.error = false;
 			return this.fromInt( parseInt( ( rgb.r << 16 ) + ( rgb.g << 8 ) + rgb.b, 10 ), preserve );
 		},
 
 		fromHex: function( color ) {
-			color = color.replace(/^#/, '');
+			color = color.replace(/^#/, '').replace(/^0x/, '');
 			if ( color.length === 3 ) {
 				color = color[0] + color[0] + color[1] + color[1] + color[2] + color[2];
 			}
+
+			// rough error checking - this is where things go squirrely the most
+			this.error = ! /^[0-9A-F]{6}$/i.test( color );
 			return this.fromInt( parseInt( color, 16 ) );
 		},
 
 		fromHsl: function( hsl ) {
+			if ( typeof hsl !== 'object' || ( ! hsl.h || ! hsl.s || ! hsl.l ) ) {
+				this.error = true;
+				return this;
+			}
+
 			var r, g, b, q, p, h, s, l;
 			this.__hsl = hsl; // store it
 			h = hsl.h / 360; s = hsl.s / 100; l = hsl.l / 100;
@@ -97,6 +112,12 @@
 
 		fromInt: function( color, preserve ) {
 			this._color = parseInt( color, 10 );
+			// let's coerce things
+			if ( this._color > 16777215 )
+				this._color = 16777215;
+			else if ( this._color < 0 )
+				this._color = 0;
+
 			if ( preserve === undef ) {
 				this.__hsl.h = this.__hsl.s = 0;
 			}
